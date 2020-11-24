d98ecc6f95a602243f0793b3b9af59a5596992af
jpassgo <jeffpascoe9@gmail.com>
2020-11-24 14:59:46 -0600

    Bare bones views to get started.

--- BarteringApp/BarteringApi/views.py
+++ BarteringApp/BarteringApi/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from . import mongodb_interface as mongodb
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse, HttpRequest
+from rest_framework.parsers import JSONParser
+from pymongo import MongoClient
+import json
 
-# Create your views here.
+@csrf_exempt
+def create_post(request):
+    return HttpResponse(status=201)
+
+
+@csrf_exempt
+def retrieve_post(request):
+    return HttpResponse(status=200)
+
+
+@csrf_exempt
+def update_post(request):
+    return HttpResponse(status=200)
+
--- BarteringApp/BarteringApp/urls.py
+++ BarteringApp/BarteringApp/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
+    path('posts/', views.create_post),
     path('admin/', admin.site.urls),
 ]
