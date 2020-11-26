cf726c888b089c51f25fa51414ecef69183a121f
jpassgo <jeffpascoe9@gmail.com>
2020-11-25 18:58:26 -0600

    Formatting

--- BarteringApp/BarteringApi/mongodb_interface.py
+++ BarteringApp/BarteringApi/mongodb_interface.py
@@ -25,12 +25,14 @@ def retrieve(id):
     posting['_id'] = str(posting.get('_id'))
     return posting
 
+
 def retrieve_postings_for_user(user_id):
     client = create_mongo_connection()
     postings_table = get_postings_table(client)
     posting = postings_table.find_one({'user_id': user_id})
     posting['user_id'] = str(posting.get('user_id'))
     return posting
+
 
 def update(id, updated_posting_attributes):
     client = create_mongo_connection()
