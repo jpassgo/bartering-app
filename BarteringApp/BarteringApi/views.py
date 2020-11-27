a8caccf9027b28ff4fc6bb11221c91e33902e5c6
jpassgo <jeffpascoe9@gmail.com>
2020-11-27 14:31:10 -0600

    Implementing retrieve postings endpoints

--- BarteringApp/BarteringApi/views.py
+++ BarteringApp/BarteringApi/views.py
@@ -3,24 +3,29 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse, HttpRequest
 from . import mongodb_interface as mongodb
 from rest_framework.parsers import JSONParser
-from pymongo import MongoClient
 import json
 
 @csrf_exempt
 def create_posting(request):
     posting = JSONParser().parse(request)
     mongodb.create(posting)
-    return HttpResponse(status=201)
+    return HttpResponse(201)
 
 
 @csrf_exempt
 def retrieve_posting(request):
-    return HttpResponse(status=200)
+    id = request.GET.get('id')
+    return HttpResponse(json.dumps(mongodb.retrieve(id)), 200)
+
+@csrf_exempt
+def retrieve_postings_for_user(request):
+    id = request.GET.get('id')
+    return HttpResponse(json.dumps(mongodb.retrieve_postings_for_user(id)), 200)
 
 
 @csrf_exempt
 def update_posting(request):
-    return HttpResponse(status=200)
+    return HttpResponse(200)
 
 
 @csrf_exempt
