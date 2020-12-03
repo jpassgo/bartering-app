e063f91c2e28597336579c65079577665bc430b9
jpassgo <jeffpascoe9@gmail.com>
2020-12-02 18:34:02 -0600

    Creating a method for converting id object returned from mongodb into a string.

--- BarteringApp/BarteringApi/mongodb_interface.py
+++ BarteringApp/BarteringApi/mongodb_interface.py
@@ -14,6 +14,7 @@ def get_postings_table(client):
 def insert_posting(user_id, posting):
     client = create_mongo_connection()
     postings_table = get_postings_table(client)
+    # Adding user_id to posting to associate the two
     posting['user_id'] = user_id
     return postings_table.insert_one(posting)
 
@@ -22,16 +23,22 @@ def retrieve(id):
     client = create_mongo_connection()
     postings_table = get_postings_table(client)
     posting = postings_table.find_one({'id': id})
-    posting['_id'] = str(posting.get('_id'))
+    convert_id_to_string(posting)
     return posting
 
 
 def retrieve_postings_for_user(user_id):
     client = create_mongo_connection()
     postings_table = get_postings_table(client)
-    postings = postings_table.find({'user_id': user_id})
+    # find returns a Cursor so we need to convert that to a list that
+    # we can return as json
+    postings = list(postings_table.find({'user_id': user_id}))
+    [convert_id_to_string(posting) for posting in postings]
     return postings
 
+def convert_id_to_string(posting):
+    posting['_id'] = str(posting.get('_id'))
+    return posting
 
 def update_posting(id, updated_posting_attributes):
     client = create_mongo_connection()
